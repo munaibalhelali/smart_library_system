9ef327f29e6f3a5eec3e5402038e2b3ce4051fa2
ASHRAF YAHYA <ashrafsadiq97@gmail.com>
2020-08-29 16:05:05 +0200

    updated login functions

--- src/login.cpp
+++ src/login.cpp
@@ -1,11 +1,7 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
 
-//write_login_data(json);
-struct read_login_data();
-/*{"name":"Ashraf", "id":1234, "type":"student"}
-data["name"]*/
  typedef struct {
   string name;
   int password;
@@ -16,67 +12,85 @@ class Login{
    char admin='a', student='s', variable1;
   
   public:
+  Login( ){}
 
+  logindaten read_login_data(string a){
+  logindaten user1;
+  user1.name = "ashraf";
+  user1.password = 12345;
+
+  return user1;
+}
   int login(){
     cout<<"type of your Account a Admin/ s Student ?"<<endl;
     cin>>variable1;
 
     if(variable1==admin){
+      cout<<"Welcom admin!"<<endl;
       logindaten user_data;
       string user_name;
       int user_password;
-      cout<<"Enter your Name: " <<endl;
+      cout<<"Enter your Name: ";
       cin>>user_name;
       try{
-      user_data = read_login_data(user_name)
+      user_data = read_login_data(user_name);
       } catch(int) {
         cout<<"User name does not exist!"<<endl;
         return 0;
       }
-      cin>>user_password;
+      cout<<"Enter the password: ";
       try{
-      user_data = read_login_data(user_password)
-      } catch(int) {
-        cout<<"User password is false!"<<endl;
-        return 0;
+        cin>>user_password;
+      }catch(int e){
+        cout<<"Execption:"<<e<<endl;
       }
+      cout<<user_data.name<<" "<<user_data.password <<endl;
       if(user_data.name==user_name && user_data.password==user_password){
-              cout<<"Welcome, Admin "<< user_name;
+              cout<<"Welcome, Admin "<< user_name<<endl;
       }
+      return 1;
 
     }
-    if(variable1==student){
+    else if(variable1==student){
     logindaten user_data;
     string user_name;
     int user_password;
-    cout<<"Enter your Name: "<<endl;
+    cout<<"Enter your Name: ";
     cin>>user_name;
     try{
-    user_data = read_login_data(user_name)
+    user_data = read_login_data(user_name);
     } catch(int) {
     cout<<"User name does not exist!"<<endl;
         return 0;
       }
 
-    cout<<"Enter your password: "<<endl;
+    cout<<"Enter your password: ";
     cin>>user_password;
-      try{
-      user_data = read_login_data(user_password)
-      } catch(int) {
-        cout<<"User password is false!"<<endl;
-        return 0;
-      }
+     
       if(user_data.name==user_name && user_data.password==user_password){
-      cout<<"Welcome, Student "<< user_name;
+      cout<<"Welcome, Student "<< user_name <<endl;
+      return 1;
      }
 
     }
   }
 
-  void to_json(){
-    //
+  string to_json(logindaten input){
+    string temp="{";
+    temp += "name:"+input.name;
+    temp += "password:"+ to_string(input.password);
+    temp += "}";
+    return temp;
   }
 };
-int main() {
-return 0;
+
+
+//testing the class
+int main(){
+  Login log;
+  
+  while(1){
+    log.login();
+  }
+  return 0;
 }
