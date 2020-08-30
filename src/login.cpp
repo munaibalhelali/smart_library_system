b5fb12c96c80394dc08cd339d2d2eec7e2f41335
munaibalhelali <moneebalhelaly@yahoo.com>
2020-08-30 21:39:46 +0200

    added login_data class

--- src/login.cpp
+++ src/login.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include <string>
+#include "login_data.h"
+
 using namespace std;
 
- typedef struct {
-  string name;
-  int password;
-}logindaten;
 
 class Login{
   private:
@@ -14,20 +12,20 @@ class Login{
   public:
   Login( ){}
 
-  logindaten read_login_data(string a){
-  logindaten user1;
-  user1.name = "ashraf";
-  user1.password = 12345;
+//   LoginData read_login_data(string a){
+//   LoginData user1;
+//   user1.name = "ashraf";
+//   user1.password = 12345;
 
-  return user1;
-}
+//   return user1;
+// }
   int login(){
     cout<<"type of your Account a Admin/ s Student ?"<<endl;
     cin>>variable1;
 
     if(variable1==admin){
       cout<<"Welcom admin!"<<endl;
-      logindaten user_data;
+      LoginData user_data;
       string user_name;
       int user_password;
       cout<<"Enter your Name: ";
@@ -45,14 +43,14 @@ class Login{
         cout<<"Execption:"<<e<<endl;
       }
       cout<<user_data.name<<" "<<user_data.password <<endl;
-      if(user_data.name==user_name && user_data.password==user_password){
+      if(user_data.get_name==user_name && user_data.get_password==user_password){
               cout<<"Welcome, Admin "<< user_name<<endl;
       }
       return 1;
 
     }
     else if(variable1==student){
-    logindaten user_data;
+    LoginData user_data;
     string user_name;
     int user_password;
     cout<<"Enter your Name: ";
@@ -67,7 +65,7 @@ class Login{
     cout<<"Enter your password: ";
     cin>>user_password;
      
-      if(user_data.name==user_name && user_data.password==user_password){
+      if(user_data.get_name==user_name && user_data.get_password==user_password){
       cout<<"Welcome, Student "<< user_name <<endl;
       return 1;
      }
@@ -75,13 +73,13 @@ class Login{
     }
   }
 
-  string to_json(logindaten input){
-    string temp="{";
-    temp += "name:"+input.name;
-    temp += "password:"+ to_string(input.password);
-    temp += "}";
-    return temp;
-  }
+  // string to_json(LoginData input){
+  //   string temp="{";
+  //   temp += "name:"+input.name;
+  //   temp += "password:"+ to_string(input.password);
+  //   temp += "}";
+  //   return temp;
+  // }
 };
 
 
