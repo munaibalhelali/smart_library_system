8f5b88aa57fce5876f64d33301a9597cd8f90168
MSH004 <68195204+MSH004@users.noreply.github.com>
2020-09-26 07:46:13 +0400

    it has been done !! .

---
+++ src/adminedriver.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<Admin.h>
+class admindriver{
+   int  check[3];
+    public:
+    
+    void adminview();
+    void student1();
+    void admin1();
+    void book1();
+};
